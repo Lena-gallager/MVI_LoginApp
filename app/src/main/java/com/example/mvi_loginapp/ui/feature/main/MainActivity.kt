439c0e12d78bf7e83519810500524523db3e06f1
Elena_M <em@m4bank.com>
2025-08-13 19:49:00 +0300

    added splash screen through androidx.core

--- app/src/main/java/com/example/mvi_loginapp/ui/feature/main/MainActivity.kt
+++ app/src/main/java/com/example/mvi_loginapp/ui/feature/main/MainActivity.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.material3.Surface
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.mvi_loginapp.ui.feature.splash.SplashViewModel
 import com.example.mvi_loginapp.ui.navigation.AppNavigation
 import com.example.mvi_loginapp.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: SplashViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        splashScreen.setKeepOnScreenCondition{viewModel.isLoading.value}
         setContent {
             AppTheme {
                 Surface(
