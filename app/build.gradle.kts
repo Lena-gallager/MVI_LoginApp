2de9f0da502080e26f8a32e721a2d2f67f5263a5
Elena_M <em@m4bank.com>
2025-08-19 21:56:04 +0300

    changed namespace for app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.mvi_loginapp"
+        applicationId = "com.example.mvi_loginapp.app"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
